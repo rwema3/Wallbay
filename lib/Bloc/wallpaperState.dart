f7ff8c4977258d9ce7a2c7c8c1fbf21d672a67eb
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-26 23:58:16 +0200

    Category Wallpaper Is Loaded

--- lib/Bloc/wallpaperState.dart
+++ lib/Bloc/wallpaperState.dart
@@ -4,5 +4,10 @@ abstract class WallpaperState {}
 
 class WallpaperIsLoading extends WallpaperState {}
 
+class CategoryWallpaperIsLoaded extends WallpaperState {
+  final List<Wallpaper> _wallpaper;
+  CategoryWallpaperIsLoaded(this._wallpaper);
+  List<Wallpaper> get getCategoryWallpaper => _wallpaper;
+}
 
 class CategoryWallpaperIsNotLoaded extends WallpaperState {}
